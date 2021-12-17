806152ef4859e958f0dc718e30e2b06129f579dc
HsuLeii <47671436+HsuLeii@users.noreply.github.com>
2021-12-17 16:59:10 +0800

    1217

--- js/all.js
+++ js/all.js
@@ -14,96 +14,154 @@ var book = document.querySelector('.book-page');
 var speaking = document.querySelector('.speak-menu-container');
 var enlarge = document.querySelector('.magnifier-menu-container');
 var text = document.querySelector('.font-menu-container');
+var read = document.querySelector('.read-menu-container');
+var vocabulary = document.querySelector('.word-menu-container');
 var page = document.querySelector('.book-page');
 var playarea = document.querySelector('.play-area');
 
-
-word.addEventListener('click', function() {
-    box.classList.toggle("open");
-    book.classList.toggle("move");
-    page.classList.remove("screen");
-    this.classList.toggle("end-word");
-  });
-arrow.addEventListener('click', function() {
-    box.classList.toggle("open");
-    book.classList.toggle("move");
-  });
-sound.addEventListener('click', function() {
-    this.classList.toggle("mute");
-  });
-verbatim.addEventListener('click', function() {
-  this.classList.toggle("end-verbatim");
+$(play).hide();
+$("#word-id").click(function() {
+  box.classList.toggle("open");
+  book.classList.toggle("move");
+  page.classList.remove("screen");
 });
-screen.addEventListener('click', function() {
+$("#read-id").click(function() {
+  $(play).toggle();
+  $('.sound-line').toggleClass("grey");
+  if ($('.sound-img').prop('disabled')) {
+    $('.sound-img').prop('disabled',false);
+  }else{
+    $('.sound-img').prop('disabled',true);
+  }
+  if ($('.btn-speak').prop('disabled')) {
+    $('.btn-speak').prop('disabled',false);
+  }else{
+    $('.btn-speak').prop('disabled',true);
+  }
+});
+
+word.addEventListener('click', function () {
+  // this.classList.toggle("end-word");
+  vocabulary.classList.toggle("open");
+  speaking.classList.remove("open");
+  enlarge.classList.remove("open");
+  read.classList.remove("open");
+  text.classList.remove("open");
+});
+arrow.addEventListener('click', function () {
+  box.classList.toggle("open");
+  book.classList.toggle("move");
+});
+sound.addEventListener('click', function () {
+  this.classList.toggle("mute");
+});
+verbatim.addEventListener('click', function () {
+  read.classList.toggle("open");
+  enlarge.classList.remove("open");
+  speaking.classList.remove("open");
+  enlarge.classList.remove("open");
+  text.classList.remove("open");
+  vocabulary.classList.remove("open");
+});
+speak.addEventListener('click', function () {
+  speaking.classList.toggle("open");
+  page.classList.remove("screen");
+  text.classList.remove("open");
+  enlarge.classList.remove("open");
+  read.classList.remove("open");
+  vocabulary.classList.remove("open");
+});
+magnifier.addEventListener('click', function () {
+  enlarge.classList.toggle("open");
+  page.classList.remove("screen");
+  speaking.classList.remove("open");
+  text.classList.remove("open");
+  read.classList.remove("open");
+  vocabulary.classList.remove("open");
+});
+font.addEventListener('click', function () {
+  text.classList.toggle("open");
+  page.classList.remove("screen");
+  speaking.classList.remove("open");
+  enlarge.classList.remove("open");
+  read.classList.remove("open");
+  vocabulary.classList.remove("open");
+});
+screen.addEventListener('click', function () {
   this.classList.toggle("end-screen");
   box.classList.remove("open");
+  document.getElementById("word-id").checked = false;
   book.classList.remove("move");
   if (page.classList.contains('screen')) {
     page.classList.remove('screen')
   } else {
     page.classList.add('screen')
   }
-  
 });
-play.addEventListener('click', function() {
+play.addEventListener('click', function () {
   this.classList.toggle("pause");
 });
-speak.addEventListener('click', function() {
-  speaking.classList.toggle("open");
-  page.classList.remove("screen");
-});
-magnifier.addEventListener('click', function() {
-  enlarge.classList.toggle("open");
-  page.classList.remove("screen");
-});
-font.addEventListener('click', function() {
-  text.classList.toggle("open");
-  page.classList.remove("screen");
-});
-favorite.addEventListener('click', function() {
+
+favorite.addEventListener('click', function () {
   this.classList.toggle("click");
 });
-if ($(window).width() < 768) { 
-    $(".sound-line").css({"width": "4px" , "height" : "40%" });
-    $("#text-id").change(function () {
-      if ($(this).is(":checked")) {
-          $(".a2-2-text p").show();
-          $(".book-page-img").css("justify-content","center");
-      } else {
-          $(".a2-2-text p").hide();
-          $(".book-page-img").css("justify-content","center");
-      }
-    }); 
-    var halftextwidth = $(".book-page-bg > img").width();     
-      $(".text").css({"max-width": halftextwidth }); 
-    var textheight = $(".a2-2-book-page-bg > img").height() / 2;     
-      $(".a2-2-text").css({"max-height" : textheight }); 
-  }
-  else if ($(window).width() > 768) {
-    var textwidth = $(".book-page-bg > img").width();     
-      $(".text").css({"max-width": textwidth }); 
-    var textheight = $(".a2-2-book-page-bg > img").height();     
-      $(".a2-2-text").css({"height": textheight }); 
+if ($(window).width() < 768) {
+  $(".sound-line").css({
+    "width": "4px",
+    "height": "40%"
+  });
+  $("#text-id").change(function () {
+    if ($(this).is(":checked")) {
+      $(".a2-2-text p").show();
+      $(".book-page-img").css("justify-content", "center");
+    } else {
+      $(".a2-2-text p").hide();
+      $(".book-page-img").css("justify-content", "center");
     }
+  });
+  var halftextwidth = $(".book-page-bg > img").width();
+  $(".text").css({
+    "max-width": halftextwidth
+  });
+  var textheight = $(".a2-2-book-page-bg > img").height() / 2;
+  $(".a2-2-text").css({
+    "max-height": textheight
+  });
+} else if ($(window).width() > 768) {
+  var textwidth = $(".book-page-bg > img").width();
+  $(".text").css({
+    "max-width": textwidth
+  });
+  var textheight = $(".a2-2-book-page-bg > img").height();
+  $(".a2-2-text").css({
+    "height": textheight
+  });
+}
 $(function () {
-  var templatetextwidth = $(".book-page-bg > img").width();     
-      $(".text").css({"width": templatetextwidth });  
-      var templatetextheight = $(".book-page-bg > img").height();     
-      $(".text").css({"height": templatetextheight }); 
+  var templatetextwidth = $(".book-page-bg > img").width();
+  $(".text").css({
+    "width": templatetextwidth
+  });
+  var templatetextheight = $(".book-page-bg > img").height();
+  $(".text").css({
+    "height": templatetextheight
+  });
   $("#pinyin-id").change(function () {
-      if ($(this).is(":checked")) {
-          $(".text p > ruby > rt").show();
-      } else {
-          $(".text p > ruby > rt").hide();
-      }
-    });
-    $("#text-id").change(function () {
-      if ($(this).is(":checked")) {
-          $(".text").show();
-      } else {
-          $(".text").hide();
-      }
-    });
-    });
+    if ($(this).is(":checked")) {
+      $(".text p > ruby > rt").show();
+    } else {
+      $(".text p > ruby > rt").hide();
+    }
+  });
+  $("#text-id").change(function () {
+    if ($(this).is(":checked")) {
+      $(".text").show();
+    } else {
+      $(".text").hide();
+    }
+  });
+  $(".text.two-right-text").css({
+    "max-width": "45%"
+  });
 
-  
+});
